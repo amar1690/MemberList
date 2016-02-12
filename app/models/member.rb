df9730066c697f3f5d7761d2a7d3fb16421c1387
amar1690 <amar@bizlinkx.com>
2016-02-12 22:50:40 +0000

    adding s3 host

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,4 @@
 class Member < ActiveRecord::Base
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :s3, :bucket => ENV["s3_bucket_name"]
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :s3, :bucket => ENV["s3_bucket_name"], :s3_host_name => "s3-us-west-1.amazonaws.com"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
